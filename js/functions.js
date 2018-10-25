90192e88c16d42236899c092bfd9279a175028cb
Calvin Smith <ceaontherock@gmail.com>
2018-10-24 19:57:58 -0700

    Made the question reappear. Changed the clunky if statement to a switch statement

--- js/functions.js
+++ js/functions.js
@@ -18,32 +18,37 @@ function onLoad(){
 }
 window.addEventListener("load", onLoad);
 
-if (page = 0) {
-  document.style.backGroundColor =
-  document.getElementById("question").innerHTML = "Something else!!!!!";
-} else if (page = 1) {
+switch (page){
+  case 1:
   document.getElementById("image").src = "assets/picture_2";
   document.getElementById("question").innerHTML = "Have you dreamed about the world ending tomorrow?";
-} else if (page = 2) {
-  document.getElementById("question").innerHTML = "Did you fail to save the world?";
-} else if (page = 3) {
-  document.getElementById("question").innerHTML = "Did you wake from this terrible dream?";
-} else if (page = 4) {
-  document.getElementById("question").innerHTML = "Have you dreamed about zombies attacking at your house?";
-} else if (page = 5) {
-  document.getElementById("question").innerHTML = "Have you dreamed about being killed in your dream?";
-} else if (page = 6) {
-  document.getElementById("question").innerHTML = "Have you dreamed about yourself flying in the sky and fall?";
-} else if (page = 7) {
-  document.getElementById("question").innerHTML = "What happened after you fall?";
-} else if (page = 8) {
-  document.getElementById("question").innerHTML = "Have you ever dreamt that you’re still in school/have a test you never prepared for?";
-} else if (page = 9) {
-  document.getElementById("question").innerHTML = "Did you pass the DUE day in that dream?";
-} else if (page = 10) {
-  document.getElementById("question").innerHTML = "Have you dreamed about you are a god?";
-} else if (page = 11) {
-  document.getElementById("question").innerHTML = "Have you dreamed about having a computer science class at 8 pm?";
-} else if (page = 12) {
+  break;
+  case 2:
 
+  break;
+  case 3:
+
+  break;
+  case 4:
+
+  break;
+  case 5:
+
+  break;
+  case 6:
+
+  break;
+  case 7:
+
+  break;
+  case 8:
+
+  break;
+  case 9:
+
+  break;
+  case 10:
+
+  break;
+  case 11:
 }
