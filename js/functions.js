ce088a36178904459ec0b9320be4cb874c6fb925
Calvin Smith <ceaontherock@gmail.com>
2018-10-24 17:14:15 -0700

    Fixed the if statements. Gave the image an id.

--- js/functions.js
+++ js/functions.js
@@ -18,29 +18,30 @@ function onLoad(){
 }
 window.addEventListener("load", onLoad);
 
-if page = 0 {
+if (page = 0) {
   document.style.backGroundColor =
-  document.question. = "Something else!!!!!";
-} else if page = 1 {
+  document.getElementById("question").innerHTML = "Something else!!!!!";
+} else if (page = 1) {
+  document.getElementById("image").src = "assets/picture_2";
+  document.getElementById("question").innerHTML = "OTHER QUESTION!!!";
+} else if (page = 2) {
 
-} else if page = 2 {
+} else if (page = 3) {
 
-} else if page = 3 {
+} else if (page = 4) {
 
-} else if page = 4 {
+} else if (page = 5) {
 
-} else if page = 5 {
+} else if (page = 6) {
 
-} else if page = 6 {
+} else if (page = 7) {
 
-} else if page = 7 {
+} else if (page = 8) {
 
-} else if page = 8 {
+} else if (page = 9) {
 
-} else if page = 9 {
+} else if (page = 10) {
 
-} else if page = 10 {
-
-} else if page = 11 {
+} else if (page = 11) {
 
 }
